aca41fd365517af288643eef7d803b2cf30354fc
sirawat <sirawat@netapp.com>
2020-04-25 00:50:37 +0700

    init

---
+++ dockerfile
@@ -0,0 +1,16 @@
+FROM node:12
+
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+RUN npm install -g artillery
+
+WORKDIR /home/node/app
+COPY package*.json ./
+
+USER node
+RUN npm install
+
+COPY --chown=node:node . .
+
+EXPOSE 3000
+
+CMD [ "node", "app.js" ]
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+
+
+var app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "13.67.107.119",
+  user: "netapp",
+  password: "Netapp1!",
+  database: "db"
+});
+
+
+
+con.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+
+function registerUser(data){
+
+    var sql = 'INSERT INTO MyGuests set ?';
+    con.query(sql,data, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+
+}
+
+app.post("/url", (req, res, next) => {
+    registerUser(req.body)
+    res.json(req.body).status(200);
+});
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+
